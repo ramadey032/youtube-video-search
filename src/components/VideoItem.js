75ba21437dc18d3eae8c85347692b732f1591b7d
ramaranjandey <36834875+ramaranjandey@users.noreply.github.com>
2020-02-23 16:43:19 +0530

    final complete app

---
+++ src/components/VideoItem.js
@@ -0,0 +1,21 @@
+import './VideoItem.css';
+import React from 'react';
+
+const VideoItem = ({ video, onVideoSelect }) => {
+    return (
+        <div className="video-item item" onClick={() => onVideoSelect(video)}>
+
+            <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.default.url} />
+            <div className="content">
+                <div className="header">
+                    {video.snippet.title}
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+
+export default VideoItem;
+
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
 const VideoList = (props) => {
-    return <div>
-        {props.videos.length} 
+    const renderedList = props.videos.map((video) => {
+        return <VideoItem key={video.id.videoId} onVideoSelect={props.onVideoSelect} video={video} />
+    });
+    return <div className="ui relaxed divided list">
+        {renderedList}
     </div>
 };
 
